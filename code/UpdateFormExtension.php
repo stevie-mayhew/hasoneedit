eac5a1170121738b966a216d13818861bcc66353
hirenpatel <hiren.patel@mademedia.co.uk>
2016-12-06 21:21:51 +0000

    Merge changes from master: cleaned up code a little

--- code/UpdateFormExtension.php
+++ code/UpdateFormExtension.php
@@ -1,27 +1,38 @@
 <?php
 
-class sgn_hasoneedit_UpdateFormExtension extends \Extension {
-	public function updateEditForm(\Form $form) {
+class sgn_hasoneedit_UpdateFormExtension extends Extension {
+
+	/**
+	 * @param Form $form
+	 */
+	public function updateEditForm(Form $form)
+	{
 		$record = $form->getRecord();
 		$fields = $form->Fields()->dataFields();
-		
-		foreach($fields as $name => $field) {
-			$name = str_replace(array(':', '/'), sgn_hasoneedit_DataObjectExtension::separator, $name);
-			if(!strpos($name, sgn_hasoneedit_DataObjectExtension::separator)) {
+
+		foreach ($fields as $name => $field) {
+			$name = str_replace(array(':', '/'), sgn_hasoneedit_DataObjectExtension::SEPARATOR, $name);
+
+			if (!strpos($name, sgn_hasoneedit_DataObjectExtension::SEPARATOR)) {
 				// Also skip $name that starts with a separator
 				continue;
 			}
+
 			$field->setName($name);
-			if(!$record) {
+
+			if (!$record) {
 				continue;
 			}
-			if($field->Value()) {
+
+			if ($field->Value()) {
 				// Skip fields that already have a value
 				continue;
 			}
-			list($hasone, $key) = explode(sgn_hasoneedit_DataObjectExtension::separator, $name, 2);
-			if($record->has_one($hasone)) {
-				$rel = $record->getComponent($hasone);
+
+			list($hasOne, $key) = explode(sgn_hasoneedit_DataObjectExtension::SEPARATOR, $name, 2);
+
+			if ($record->hasOne($hasOne) || $record->belongsTo($hasOne)) {
+				$rel = $record->getComponent($hasOne);
 				// Copied from loadDataFrom()
 				$exists = (
 					isset($rel->$key) ||
@@ -29,7 +40,7 @@ class sgn_hasoneedit_UpdateFormExtension extends \Extension {
 					($rel->hasMethod('hasField') && $rel->hasField($key))
 				);
 
-				if($exists) {
+				if ($exists) {
 					$value = $rel->__get($key);
 					$field->setValue($value);
 				}
@@ -37,7 +48,10 @@ class sgn_hasoneedit_UpdateFormExtension extends \Extension {
 		}
 	}
 
-	public function updateItemEditForm(\Form $form) {
+	/**
+	 * @param Form $form
+	 */
+	public function updateItemEditForm(Form $form) {
 		$this->updateEditForm($form);
 	}
 }
