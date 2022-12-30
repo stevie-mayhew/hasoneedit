af6e7e82b05cdc2646eddc40098220d4c9f8b746
Nicolaas / Sunny Side Up <github@sunnysideup.co.nz>
2022-12-31 02:07:07 +1300

    MINOR: allow restrictions to DB fields only

--- src/ProvidesHasOneInlineFields.php
+++ src/ProvidesHasOneInlineFields.php
@@ -13,12 +13,17 @@ trait ProvidesHasOneInlineFields
      * @param string $relationName
      * @return \SilverStripe\Forms\FieldList|\SilverStripe\Forms\FormField[]
      */
-    public function provideHasOneInlineFields($relationName)
+    public function provideHasOneInlineFields(string $relationName, ?bool $dbFieldsOnly = false)
     {
         $fields = $this->getCMSFields()->dataFields();
-
+        $restrictions = [];
+        if ($dbFieldsOnly) {
+            $restrictions = $this->Config()->get('db');
+        }
         foreach ($fields as $name => $field) {
-            $field->setName($relationName . HasOneEdit::FIELD_SEPARATOR . $field->getName());
+            if (empty($restrictions) || in_array($name, $restrictions)) {
+                $field->setName($relationName . HasOneEdit::FIELD_SEPARATOR . $field->getName());
+            }
         }
 
         return $fields;
